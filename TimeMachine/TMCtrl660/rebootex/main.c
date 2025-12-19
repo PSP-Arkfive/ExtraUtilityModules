b3ab51dbe97e0b9131a59ade0eac7ebe0bce2007
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2025-12-19 21:45:08 +0100

    fixes

--- TimeMachine/TMCtrl660/rebootex/main.c
+++ TimeMachine/TMCtrl660/rebootex/main.c
@@ -5,6 +5,18 @@
 #include <systemctrl_se.h>
 #include <bootloadex.h>
 
+#include <fat.h>
+#include <syscon.h>
+
+#define REG32(addr) *((volatile uint32_t *)(addr))
+
+static ExtraIoFuncs ms_extra_io = {
+    .FatMount = &MsFatMount,
+    .FatOpen = &MsFatOpen,
+    .FatRead = &MsFatRead,
+    .FatClose = &MsFatClose,
+};
+
 // Entry Point
 int cfwBoot(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7)
 {
@@ -13,6 +25,15 @@ int cfwBoot(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7
     #endif
 
     is_msipl = 1;
+    extra_io = &ms_extra_io;
+
+    // GPIO enable
+    REG32(0xbc10007c) |= 0xc8;
+    __asm("sync"::);
+    
+    syscon_init();
+    
+    syscon_ctrl_ms_power(1);
 
     // check config
     checkRebootConfig();
