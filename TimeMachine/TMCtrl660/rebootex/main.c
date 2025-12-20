fbb71c4991729e3c8cfa0090078f72221b1225ca
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2025-12-20 02:49:00 +0100

    update

--- TimeMachine/TMCtrl660/rebootex/main.c
+++ TimeMachine/TMCtrl660/rebootex/main.c
@@ -10,11 +10,13 @@
 
 #define REG32(addr) *((volatile uint32_t *)(addr))
 
-static ExtraIoFuncs ms_extra_io = {
-    .FatMount = &MsFatMount,
-    .FatOpen = &MsFatOpen,
-    .FatRead = &MsFatRead,
-    .FatClose = &MsFatClose,
+ExtraIoFuncs iofuncs = {
+    .psp_io = {
+        .FatMount = &MsFatMount,
+        .FatOpen = &MsFatOpen,
+        .FatRead = &MsFatRead,
+        .FatClose = &MsFatClose,
+    }
 };
 
 // Entry Point
@@ -24,25 +26,21 @@ int cfwBoot(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7
     colorDebug(0xff00);
     #endif
 
-    is_msipl = 1;
-    extra_io = &ms_extra_io;
-
     // GPIO enable
     REG32(0xbc10007c) |= 0xc8;
     __asm("sync"::);
     
     syscon_init();
-    
     syscon_ctrl_ms_power(1);
 
-    // check config
-    checkRebootConfig();
+    // Configure
+    bootConfig(MS_BOOT, TYPE_REBOOTEX, &iofuncs);
 
-    // scan for reboot functions
-    findRebootFunctions();
+    // scan functions
+    findBootFunctions();
     
-    // patch reboot buffer
-    patchRebootBufferPSP();
+    // patch sceboot
+    patchBootPSP();
     
     // Forward Call
     return sceReboot(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
