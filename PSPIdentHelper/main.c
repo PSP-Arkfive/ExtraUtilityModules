fc2399469e93f92ab05229d8c0d56241fc3182e8
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2025-12-20 21:52:50 +0100

    cleanup

--- PSPIdentHelper/main.c
+++ PSPIdentHelper/main.c
@@ -1,39 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include <pspsdk.h>
 #include <pspkernel.h>
 #include <pspdebug.h>
 #include <psploadcore.h>
 #include <pspiofilemgr.h>
 #include <pspdisplay.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 
 PSP_MODULE_INFO("kpspident", 0x1006, 1, 0);
 PSP_MAIN_THREAD_ATTR(0);
 
-u32 sceSyscon_driver_7EC5A957(u32 *baryon);
-u32 sceSyscon_driver_E7E87741(u32 *pommel);
-int sceSysreg_driver_E2A5D1EE(void);
-int sceSysregKirkBusClockEnable(void);
-int sceSysregAtaBusClockEnable(void);
 
 int sceSysconGetBaryonVersion(u32 *baryon) {
     int k1 = pspSdkSetK1(0);
-    int err = sceSyscon_driver_7EC5A957(baryon);
+    int err = sceSysconGetBaryonVersion(baryon);
     pspSdkSetK1(k1);
     return err;
 }
 
 int sceSysconGetPommelVersion(u32 *pommel) {
     int k1 = pspSdkSetK1(0);
-    int err = sceSyscon_driver_E7E87741(pommel);
+    int err = sceSysconGetPommelVersion(pommel);
     pspSdkSetK1(k1);
     return err;
 }
 
 int sceSysregGetTachyonVersion(void) {
     int k1 = pspSdkSetK1(0);
-    int err = sceSysreg_driver_E2A5D1EE();
+    int err = sceSysregGetTachyonVersion();
     pspSdkSetK1(k1);
     return err;
 }
